6d7e33ad21393aa04b6af30845853509beffd283
IPAYMU DEV <ipaymudev@IPAYMUs-MacBook-Pro.local>
2023-05-19 10:07:07 +0800

    update direct payment test

--- example/payment/virtual_account.go
+++ example/payment/virtual_account.go
@@ -11,12 +11,17 @@ func DirectVirtualAccount() error {
 	client.ApiKey = "QbGcoO0Qds9sQFDmY0MWg1Tq.xtuh1"
 	client.VirtualAccount = "1179000899"
 
+	notifyUrl := "http://localhost/notify-url"
+	expired := int8(24)
+	expiredType := ipaymu.Hours
+	referenceId := "trx-123"
+
 	request := ipaymu.NewRequestDirectVA(ipaymu.CimbNiaga)
-	request.AddBuyer("buyer", "phone", "email@test.com")
-	request.NotifyUrl = "http://localhost/notify-url"
-	request.Expired = 24
-	request.ExpiredType = ipaymu.Hours
-	request.ReferenceId = "trx-123"
+	request.AddBuyer("buyer", "08123123123", "email@test.com")
+	request.NotifyUrl = &notifyUrl
+	request.Expired = &expired
+	request.ExpiredType = &expiredType
+	request.ReferenceId = &referenceId
 	request.Amount = 100000
 
 	va, err := client.DirectPaymentVA(*request)
