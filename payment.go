eeb599df437830f88a30568452020314ba526171
zian <mynameiszian@gmail.com>
2024-03-08 20:28:34 +0800

    feat: added direct payment

--- payment.go
+++ payment.go
@@ -42,7 +42,32 @@ func (c Client) DirectPaymentVA(request RequestDirectVA) (res Response, err erro
 		return Response{}, err
 	}
 
-		res = responseUpper.ToResponse()
+	res = responseUpper.ToResponse()
+
+	if res.Status != 200 {
+		return res, fmt.Errorf("%s", res.Message)
+	}
+
+	return
+}
+
+func (c Client) DirectPayment(request RequestDirectMaster) (res Response, err error) {
+	url, _ := url.Parse(fmt.Sprintf("%s/api/v2/payment/direct", c.EnvApi))
+	jsonBody, _ := json.Marshal(request)
+	signature := fmt.Sprintf("%s", GenerateSignature(string(jsonBody), "POST", c))
+	api, err := c.CallApi(url, signature, jsonBody)
+	if err != nil {
+		return Response{}, err
+	}
+
+	var responseUpper ResponseUpper
+
+	err = json.Unmarshal(api, &responseUpper)
+	if err != nil {
+		return Response{}, err
+	}
+
+	res = responseUpper.ToResponse()
 
 	if res.Status != 200 {
 		return res, fmt.Errorf("%s", res.Message)
